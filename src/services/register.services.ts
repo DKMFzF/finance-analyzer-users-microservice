342101097c43f0cb8abbf75f61403f2889068b64
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-28 03:57:13 +0700

    update register.services: add docs

--- src/services/register.services.ts
+++ src/services/register.services.ts
@@ -11,14 +11,13 @@ const SQL_REGISTER_USER = 'INSERT INTO users (login, password) VALUES ($1, $2) R
 export const registerUser = async (login: string, password: string) => {
   logger.info('[LOG]: start service registerUser');
 
-  // Проверяем, есть ли пользователь с таким login
   const userExists = await pool.query(SQL_CHECK_USER_EXISTS, [login]);
 
   if (userExists.rows.length > 0) {
+    logger.error('[ERROR]: user with login %s already exists', login);
     throw new Error('Пользователь с таким логином уже существует');
   }
 
-  // Если пользователя нет — регистрируем
   const result = await pool.query(SQL_REGISTER_USER, [login, password]);
 
   logger.info('[LOG]: end service registerUser');
