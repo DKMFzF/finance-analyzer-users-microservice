023f4526401196e2b575e749ff89b38be473b395
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-28 03:48:08 +0700

    fix db bag autho inctement

--- src/services/register.services.ts
+++ src/services/register.services.ts
@@ -5,13 +5,25 @@ import logger from '../utils/logger/logger';
  * регистрация пользователя
  */
 
-const SQL_REQUEST_REGISTER_USER = 'INSERT INTO users (login, password) VALUES ($1, $2) RETURNING *';
+const SQL_CHECK_USER_EXISTS = 'SELECT id FROM users WHERE login = $1';
+const SQL_REGISTER_USER = 'INSERT INTO users (login, password) VALUES ($1, $2) RETURNING *';
 
 export const registerUser = async (login: string, password: string) => {
   logger.info('[LOG]: start service registerUser');
-  const result = await pool.query(SQL_REQUEST_REGISTER_USER, [login, password]);
+
+  // Проверяем, есть ли пользователь с таким login
+  const userExists = await pool.query(SQL_CHECK_USER_EXISTS, [login]);
+
+  if (userExists.rows.length > 0) {
+    throw new Error('Пользователь с таким логином уже существует');
+  }
+
+  // Если пользователя нет — регистрируем
+  const result = await pool.query(SQL_REGISTER_USER, [login, password]);
+
   logger.info('[LOG]: end service registerUser');
   return result.rows[0];
 };
 
 export default registerUser;
+
