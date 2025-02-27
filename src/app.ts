12687d482a4404bd55c5359ee54b604cffb9fc2e
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-28 03:58:30 +0700

    add logs init in app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import router from './routes/users.routes';
+import logger from "./utils/logger/logger";
+import LOGS from "./utils/custom.logs";
 
 /**
  * init sevice
@@ -9,5 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/', router);
+
+logger.info(LOGS.SERVICE_INIT);
 
 export default app;
